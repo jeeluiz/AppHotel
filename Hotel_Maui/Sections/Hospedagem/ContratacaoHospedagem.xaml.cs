3e5cf40947c1946ab86dce6750834847037720d8
Jeferson Santos <jeferson.santos@onfriday.com.br>
2022-08-12 15:55:54 -0300

    logo

--- Hotel_Maui/Sections/Hospedagem/ContratacaoHospedagem.xaml.cs
+++ Hotel_Maui/Sections/Hospedagem/ContratacaoHospedagem.xaml.cs
@@ -6,13 +6,10 @@ namespace Hotel_Maui.View;
 public partial class ContratacaoHospedagem : ContentPage
 {
     public ContratacaoHospedagem()
-	{
+    {
         BindingContext = new ContratacaoHospedagemViewModel();
         InitializeComponent();
     }
 
-    private void DatePicker_BindingContextChanged(object sender, EventArgs e)
-    {
 
-    }
 }
